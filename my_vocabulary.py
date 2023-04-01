ca9e1f6fd0307b5852537189a18ca4451bbee1ed
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2023-04-01 20:32:08 +0600

    vocabulary 2

---
+++ my_vocabulary.py
@@ -0,0 +1,11 @@
+vocabulary = {
+    'ahkatlio': {
+        'numeric_value': 1123,
+        'english_meaning': 'example'
+    },
+    'ihaoak': {
+        'numeric_value': 3.1416,
+        'english_meaning': 'pi'
+    },
+    
+}
